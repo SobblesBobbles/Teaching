01f326b423de6cea0bc599204dfdc92e095a83cd
SobblesBobbles <obrienstephen21@gmail.com>
2018-06-19 20:37:08 +0100

    Virtual

---
+++ C++/VirtualExample/Charmander.h
@@ -0,0 +1,22 @@
+//
+// Created by Stephen O Brien on 19/06/2018.
+//
+
+#ifndef VIRTUALEXAMPLE_CHARMANDER_H
+#define VIRTUALEXAMPLE_CHARMANDER_H
+#include "Pokemon.h"
+
+
+class Charmander : public Pokemon{
+
+public:
+    Charmander();
+
+    virtual void Eat();
+
+
+
+};
+
+
+#endif //VIRTUALEXAMPLE_CHARMANDER_H
